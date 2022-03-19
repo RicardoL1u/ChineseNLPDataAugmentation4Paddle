339bf0d5c3276548e56c80b7c3d74d003795f3ef
RicardoL1u <2018212874@bupt.edu.cn>
2022-03-19 11:11:37 +0800

    refactor: fill mask

--- test_fillmask.py
+++ test_fillmask.py
@@ -10,32 +10,24 @@ class TestMask(unittest.TestCase):
         super().__init__(methodName)
         self.model = BertForMaskedLM.from_pretrained('bert-base-chinese')
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
-        self.mask_token_id = 103
     def test_fill(self):
-        test_str = '巴黎是[MASK]国的首都.'
+        test_str = '在这篇文章中，我会努力去概述[MASK]当前用于文本数据增强的方法.'
         model_inputs = self.tokenizer(test_str)
         input_ids = model_inputs["input_ids"]
-        model_inputs = {k:paddle.to_tensor([v]) for (k, v) in model_inputs.items()}
         # print(type(model_inputs),model_inputs)
-        model_outputs = self.model(**model_inputs)
-        print(model_inputs)
-        print(model_outputs.shape)
-
-
-        print("\n======================fsdfadfasdfsdfasdf======================================\n")
+        model_outputs = self.model(**{k:paddle.to_tensor([v]) for (k,v) in model_inputs.items()})
+        print("\n========================This Main======================================\n")
         top_k = 5
         outputs = model_outputs
-        masked_list = np.squeeze(self.mask_token_id == np.array(model_inputs["input_ids"]))
+        masked_list = np.squeeze(self.tokenizer.mask_token_id == np.array(input_ids))
         masked_index = paddle.nonzero(paddle.to_tensor(masked_list,dtype='int32'), as_tuple=False).squeeze(axis=-1).tolist()
         # Fill mask pipeline supports only one ${mask_token} per sample
         outputs = outputs.numpy()
         logits = paddle.to_tensor(outputs[0, masked_index, :])
         probs = paddle.nn.functional.softmax(logits,axis=-1)
-
         values, predictions = probs.topk(top_k)
 
         result = []
-        single_mask = values.shape[0] == 1
         for i, (_values, _predictions) in enumerate(zip(values.tolist(), predictions.tolist())):
             row = []
             for v, p in zip(_values, _predictions):
@@ -47,7 +39,7 @@ class TestMask(unittest.TestCase):
                 # Originally we skip special tokens to give readable output.
                 # For multi masks though, the other [MASK] would be removed otherwise
                 # making the output look odd, so we add them back
-                sequence = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(tokens,skip_special_tokens=single_mask))
+                sequence = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(tokens,skip_special_tokens=(values.shape[0] == 1)))
                 proposition = {"score": v, "token": p, "token_str": self.tokenizer.convert_ids_to_tokens(p), "sequence": sequence}
                 row.append(proposition)
             result.append(row)
